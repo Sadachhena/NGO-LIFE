5b33713421b9cf8c7f3644709c55ffbb1fd907a2
Sada Hussain <sadachhena@gmail.com>
2026-05-24 09:59:04 -0700

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -30,7 +30,26 @@ export default function Header() {
       <div></div>
       {/* {Second div} */}
       <div className="flex items-center justify-between">
-            
+     {/* {Logo} */}
+     <div>
+        <h2>Logo</h2>
+     </div>
+     {/* {NavLink} */}
+     <div>
+        <ul className="flex justify-center items-center gap-5">
+            {Nav.map((items,i)=>{
+                return(
+                    <li key={i}>
+                     {items.name}
+                    </li>
+                )
+            })}
+        </ul>
+     </div>
+     {/* {DonoteButton} */}
+     <div>
+        <button className="bg-orange-400 text-xl rounded-lg">Donote</button>
+     </div>
       </div>  
     </header>
   )
