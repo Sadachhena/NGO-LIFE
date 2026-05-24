dd085e38cc3ac676803aaeb2a5aeaccedf3da58c
Sada Hussain <sadachhena@gmail.com>
2026-05-24 10:37:14 -0700

    Topbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,6 @@
+import { FaFacebook,  FaMailBulk,  FaPinterest,  FaTwitter, FaYoutube } from "react-icons/fa"
+import { FaLocationPin } from "react-icons/fa6"
+import { IoIosCall } from "react-icons/io";
 let Nav=[
     {
         name:"Home"
@@ -25,11 +28,59 @@ let Nav=[
 
 export default function Header() {
   return (
-    <header className="max-w-[1300px] mx-auto px-10 shadow-lg p-2">
-      {/* {First div} */}
-      <div></div>
+    <header className="w-full mx-auto px-10 shadow-lg p-2 flex-col ">
+      {/* Top Bar */}
+<div className="bg-[#6a0d5c] text-white flex justify-between items-center px-8 py-3 text-sm">
+
+  {/* Contact Info */}
+  <div className="flex items-center gap-8">
+
+    <span className="flex items-center gap-2">
+      <FaLocationPin className="text-orange-400 text-[14px]" />
+      <h3 className="text-[14px] font-medium">
+        Pattoki, Punjab College
+      </h3>
+    </span>
+
+    <span className="flex items-center gap-2">
+      <IoIosCall className="text-orange-400 text-[16px]" />
+      <h3 className="text-[14px] font-medium">
+        123-4567890
+      </h3>
+    </span>
+
+    <span className="flex items-center gap-2">
+      <FaMailBulk className="text-orange-400 text-[14px]" />
+      <h3 className="text-[14px] font-medium">
+        admin@gmail.com
+      </h3>
+    </span>
+
+  </div>
+
+  {/* Social Icons */}
+  <div className="flex items-center gap-3">
+
+    <span className="w-8 h-8 rounded-full bg-white text-orange-500 flex justify-center items-center cursor-pointer hover:scale-105 duration-300">
+      <FaFacebook />
+    </span>
+
+    <span className="w-8 h-8 rounded-full bg-white text-orange-500 flex justify-center items-center cursor-pointer hover:scale-105 duration-300">
+      <FaTwitter />
+    </span>
+
+    <span className="w-8 h-8 rounded-full bg-white text-orange-500 flex justify-center items-center cursor-pointer hover:scale-105 duration-300">
+      <FaYoutube />
+    </span>
+
+    <span className="w-8 h-8 rounded-full bg-white text-orange-500 flex justify-center items-center cursor-pointer hover:scale-105 duration-300">
+      <FaPinterest />
+    </span>
+
+  </div>
+</div>
       {/* {Second div} */}
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between py-5">
      {/* {Logo} */}
      <div>
         <h2>Logo</h2>
